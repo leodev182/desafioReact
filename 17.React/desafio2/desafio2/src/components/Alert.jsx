4b2f3c08b85a404ccd7eb1c73ae5699db528e18e
Mariana García Hoffmann <mariana.ghoffmann18@gmail.com>
2023-10-29 18:02:03 -0600

    se modificaron los alert dentro de las validaciones

--- 17.React/desafio2/desafio2/src/components/Alert.jsx
+++ 17.React/desafio2/desafio2/src/components/Alert.jsx
@@ -1,7 +1,1 @@
-/*import { Alert as AlertBootstrap } from "react-bootstrap/Alert";
 
-const Alert = ({ mensaje }) => {
-  return <AlertBootstrap variant="">{mensaje}</AlertBootstrap>;
-};
-
-export default Alert;*/
